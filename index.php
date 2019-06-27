c22f260720fa01f942c790ad5ad1a5a1660e4c56
Ismael-Oliveira <oliveiraismael2@gmail.com>
2019-06-27 18:34:05 -0300

    Upload de varios arquivos

--- index.php
+++ index.php
@@ -1,22 +1,32 @@
+
 <?php
+    // echo "<pre>";
+    // print_r($_FILES);
 
     if(isset($_FILES['arquivo'])){
-        $file = $_FILES['arquivo'];
-        if(isset($file['tmp_name']) && !empty($file['tmp_name'])){
-            $namefile = md5(time().rand(0,999)).".png";
-            // echo $namefile; exit;
-            move_uploaded_file($file['tmp_name'], 'arquivos/'.$namefile);
-            echo "Arquivo enviado com sucesso.";
+        $files = $_FILES['arquivo'];
+
+        if(count($files['tmp_name']) > 0){
+
+            for ($i=0; $i < count($files['tmp_name']); $i++) { 
+
+                $ext = explode(".",$files['name'][$i]);
+                //print_r($ext); exit;
+                $new_name = md5(time().rand(0,999)).".".$ext[1];
+                
+                if($ext != 'php'){
+                    move_uploaded_file($files['tmp_name'][$i],'arquivos/'.$new_name);
+                }
+            }
+            echo "Arquivos enviados com sucesso.";            
         }
     }
-
-    // echo "<pre>";
-    // var_dump($file);
-
 ?>
-<form method="POST" enctype="multipart/form-data">
 
-    <input type="file" name="arquivo"><br/><br/>
+<form method="POST" enctype="multipart/form-data">
+    
+    Arquivo: <br>
+    <input type="file" name="arquivo[]" multiple><br/><br/>
     <button type="submit">Adicionar</button>
 </form>
 
